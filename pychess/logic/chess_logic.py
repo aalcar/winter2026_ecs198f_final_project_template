5e77867702b316cfde31f2e15552dcfb3275562e
aalcar <alcarazalex2006@gmail.com>
2026-03-19 03:51:19 -0700

    fix castling bug

--- pychess/logic/chess_logic.py
+++ pychess/logic/chess_logic.py
@@ -87,11 +87,36 @@ class ChessLogic:
             return True
         
         return False
+
+    def is_square_attacked(self, row: int, col: int, by_player: str) -> bool:
+        original_player = self.current_player
+        self.current_player = by_player
+
+        # can any opposing pieces move to this square
+        for piece_row in range(8):
+            for piece_col in range(8):
+                if self.is_own_piece(piece_row, piece_col):
+                    possible_moves = self._get_valid_moves_for_piece(piece_row, piece_col)
+                    if (row, col) in possible_moves:
+                        self.current_player = original_player
+                        return True
+                    
+        self.current_player = original_player
+        return False
     
-    def update_pieces_moved(self, moving_piece: str, row: int, col: int):
-        if moving_piece.lower() == "k":
-            self.pieces_moved[(row, col)] = True
-        elif moving_piece.lower() == "r":
+    def is_king_in_check(self, player: str) -> bool:
+        king = "K" if player == "White" else 'k'
+
+        for row in range(8):
+            for col in range(8):
+                if self.board[row][col] == king:
+                    opponent = "Black" if player == "White" else "White"
+                    return self.is_square_attacked(row, col, opponent)
+        
+        return False
+    
+    def update_pieces_moved(self, row: int, col: int):
+        if (row, col) in self.pieces_moved:
             self.pieces_moved[(row, col)] = True
     
     def _build_valid_moves(self):
@@ -144,11 +169,22 @@ class ChessLogic:
                 if self.board[two_forward[0]][two_forward[1]] == "":
                     valid_moves.append(two_forward)
         
+        # captures
         for diagonal in [-1, 1]:
             potential_capture = (row + direction, col + diagonal)
             if self.is_in_bounds(potential_capture[0], potential_capture[1]) \
             and self.is_opp_piece(potential_capture[0], potential_capture[1]):
                 valid_moves.append(potential_capture)
+
+        # en passant
+        if self.last_move is not None:
+            last_start_row, last_start_col, last_end_row, last_end_col = self.last_move
+            last_piece = self.board[last_end_row][last_end_col]
+            
+            if last_piece.lower() == "p" and abs(last_start_row - last_end_row) == 2 \
+            and last_end_col in [col - 1, col + 1] \
+            and last_end_row == row:
+                valid_moves.append((row + direction, last_end_col))
 
         return valid_moves
     
@@ -230,13 +266,23 @@ class ChessLogic:
                 valid_moves.append((new_row, new_col))
 
         # castling
+        # TODO can king castle while in check?
+        if self.is_king_in_check(self.current_player):
+            return valid_moves
+
+        opponent = "Black" if self.current_player == "White" else "White"
         pos = (row, col)
         if not self.pieces_moved[pos]: # castling is available
             # queenside
-            if not self.pieces_moved[(row, 0)] and all(self.board[row][c] == "" for c in [1, 2, 3]):
+            if not self.pieces_moved[(row, 0)] \
+            and all(self.board[row][c] == "" for c in [1, 2, 3]) \
+            and not self.is_square_attacked(row, 3, opponent) \
+            and not self.is_square_attacked(row, 2, opponent):
                 valid_moves.append((row, 2))
             # kingside
-            if not self.pieces_moved[(row, 7)] and all(self.board[row][c] == "" for c in [5, 6]):
+            if not self.pieces_moved[(row, 7)] \
+            and all(self.board[row][c] == "" for c in [5, 6]) \
+            and not self.is_square_attacked(row, 5, opponent):
                 valid_moves.append((row, 6))
 
         return valid_moves
@@ -270,11 +316,20 @@ class ChessLogic:
         
         # initiate move
         moving_piece = self.board[start_row][start_col]
+
         is_capture = self.board[end_row][end_col] != ""
+        is_castling = moving_piece.lower() == "k" and abs(start_col - end_col) == 2
+        is_en_passant = moving_piece.lower() == "p" and is_capture and self.board[end_row][end_col] == ""
         is_promotion = self.is_move_promotion(moving_piece, end_row)
 
+        # save incase move sequence causes our king to be in check
+        original_board = [row[:] for row in self.board]
+
+        # update board
+        self.board[start_row][start_col] = ""
+        self.board[end_row][end_col] = moving_piece
+
         # castling check here
-        is_castling = moving_piece.lower() == "k" and abs(start_col - end_col) == 2
         if is_castling:
             if end_col > start_col: # kingside
                 self.board[start_row][7] = ""
@@ -285,15 +340,18 @@ class ChessLogic:
                 self.board[start_row][3] = "R" if self.current_player == "White" else "r"
                 notation = "0-0-0"
 
-        is_en_passant = moving_piece.lower() == "p" and is_capture and self.board[end_row][end_col] == ""
         if is_en_passant:
             # start row is where the opp. pawn is
             is_capture = True
             self.board[start_row][end_col] = ""
 
-        # update board
-        self.board[start_row][start_col] = ""
-        self.board[end_row][end_col] = moving_piece
+        if is_promotion:
+            self.board[end_row][end_col] = "Q" if self.current_player == "White" else "q"
+
+        # undo if king in check from this move, invalid move
+        if self.is_king_in_check(self.current_player):
+            self.board = original_board
+            return ""
 
         # construct notation
         if not is_castling:
@@ -308,7 +366,7 @@ class ChessLogic:
                 notation += "=Q"
         
         # update properties
-        self.update_pieces_moved(moving_piece, start_row, start_col)
+        self.update_pieces_moved(start_row, start_col)
 
         self.last_move = (start_row, start_col, end_row, end_col)
         self.current_player = "Black" if self.current_player == "White" else "White"
